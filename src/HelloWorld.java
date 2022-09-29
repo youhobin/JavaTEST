ca2ea2a02f43ec1320e6ba3075913c1e1e9c878e
youhobin <ghdb132@naver.com>
2022-09-29 10:00:25 +0900

    hello world 메시지 추가

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,6 +5,8 @@ public class HelloWorld {
 		// TODO Auto-generated method stub
 
 			System.out.println("Hello World");
+			System.out.println("Hello World");
+			System.out.println("Hello World");
 	}
 
 }
